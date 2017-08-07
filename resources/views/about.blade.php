296de59fc38d7997660eb27ee95c18ee70e89f23
Slawa Gladkov <slawa.gladkov@invision.de>
2017-08-07 16:57:20 +0200

    Add About view and route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,2 @@
+<h2>About page</h2>
+<p>There are over {{ $number_of_cats }} cats on this site!</p>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+  return redirect('/cats');
+});
+
+Route::get('/cats', function(){
+  return 'All cats in a family';
+});
+
+Route::get('/cats/{id}', function($id) {
+  return sprintf('Cat #%s', $id);
+})->where('id', '[0-9]+');
+
+Route::get('/about',  function(){
+  return view('about')->with('number_of_cats', 432);
 });

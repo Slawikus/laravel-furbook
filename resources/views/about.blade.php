e678400efade29d1207502be97d626f799154176
Slawa Gladkov <slawa.gladkov@invision.de>
2017-08-08 08:34:07 +0200

    Add boostrap and use it in About view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,7 @@
-<h2>About page</h2>
-<p>There are over {{ $number_of_cats }} cats on this site!</p>
+@extends('layouts.master')
+@section('header')
+    <h2>About page</h2>
+@endsection
+@section('content')
+    <p>There are over {{ $number_of_cats }} cats on this site!</p>
+@endsection
